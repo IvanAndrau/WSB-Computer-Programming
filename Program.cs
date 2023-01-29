d1efa8f9526465606cccd47381b87d3e175dbf60
Ivan <81695671+IvanAndrau@users.noreply.github.com>
2023-01-29 22:59:11 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+var text = new StringBuilder();
+bool start = false;
+Console.WriteLine("Write 'start' to start writing journal and 'stop' to stop writing");
+while (true)
+{
+   var input = Console.ReadLine();
+    if(input == "stop")
+    {
+        text.Append("Jean-Luc Picard");
+        break; 
+    }
+
+    if (input == "start")
+    {
+        start = true;
+        text.Append("Captain's log");
+        text.Append("Stardate <" + DateTime.Now.ToString("MM/dd/yyyy") + ">");
+        continue;
+    }
+
+    if (start)  
+        text.Append(input + "\n"); 
+    else
+        Console.WriteLine("You'r not writing to your Journal\n");
+}
+string path = DateTime.Now.ToString("MMddyyyy") + ".txt";
+File.WriteAllText(path, text.ToString());
